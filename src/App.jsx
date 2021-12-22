819121dcc4be0ab7f56be2b1e042ed4bc141adf1
bekauz <bekauz@protonmail.com>
2021-12-22 21:46:50 +0100

    option to connect with wallet; updated gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useMemo, useState } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // connectWallet hook from thirdWeb
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  // if user hasn't connected the wallet, display an option to do so
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to OctopusDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  
+  // once user is connected display landing page
   return (
     <div className="landing">
-      <h1>Welcome to Octopus DAO</h1>
-    </div>
-  );
+      <h1>wallet connected, welcome!</h1>
+    </div>);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
 
-// Render the App component to the DOM
+/**
+ * 1 = mainnet
+ * 3 = ropsten
+ * 4 = rinkeby
+ */
+const supportedChainIds = [4];
+
+/**
+ * Include the connectors you want to support
+ * injected - MetaMask
+ * magic - Magic Link
+ * walletconnect - Wallet Connect
+ * walletlink - Coinbase Wallet
+ */
+const connectors = {
+  injected: {},
+};
+
+// wrap App with ThirdwebWeb3Provider
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <div className="landing">
+        <App />
+      </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
